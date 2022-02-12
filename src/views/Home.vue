9c20b7c7f844e5003dde38ecd2f811a256baaff1
kadek <kadek@gotomalls.com>
2022-02-12 19:51:32 +0800

    add tailwindcss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
+    <!--
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    !-->
+
+    <p>welcome home</p>
   </div>
 </template>
 
